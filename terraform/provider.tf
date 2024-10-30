9a075fbbe89900ac24c6062ed36b848253f9a6d6
Jeremy Harrington <jerms13@gmail.com>
2024-10-29 19:19:03 -0700

    fix: aws provider required

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-fcxp32"
-    key    = "terraform/terraform.tfstate"
-    region = "us-west-1"
-  }
-}
-
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
@@ -15,6 +7,20 @@ provider "aws" {
     tags = {
       "Terraform" = "true"
     }
+  }
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-state-fcxp32"
+    key    = "terraform/terraform.tfstate"
+    region = "us-west-1"
   }
 }
 
