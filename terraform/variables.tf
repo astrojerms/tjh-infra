2793376d55c6152d38f59df480459df440c1bee1
Jeremy Harrington <jerms13@gmail.com>
2024-10-29 19:22:57 -0700

    fix: formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -69,19 +69,19 @@ variable "vpc_name" {
 }
 
 variable "private_subnets" {
-    description = "CIDR blocks for the private subnets"
-    type        = list(string)
-    default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnets" {
-    description = "CIDR blocks for the public subnets"
-    type        = list(string)
-    default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  description = "CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "vpc_cidr" {
-    description = "CIDR block for the VPC"
-    type        = string
-    default     = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
